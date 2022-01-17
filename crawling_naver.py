dcbe0d630e7f075282cc66e152e53c26e284a940
unknown <tndwns19@gmail.com>
2022-01-17 20:16:00 +0900

    multi + list append+ 90seconds

--- crawling_naver.py
+++ crawling_naver.py
@@ -1,115 +1,114 @@
-from multiprocessing.spawn import freeze_support
-import requests
 from bs4 import BeautifulSoup
-import time
-import urllib.request
-from multiprocessing import Pool,  Manager  # Pool import하기
+from datetime import datetime
+import requests
+import pandas as pd
+import re
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+< naver 뉴스 검색시 리스트 크롤링하는 프로그램 > _select사용
+- 크롤링 해오는 것 : 링크,제목,신문사,날짜,내용요약본
+- 날짜,내용요약본  -> 정제 작업 필요
+- 리스트 -> 딕셔너리 -> df -> 엑셀로 저장 
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-url = "http://api.seibro.or.kr/openapi/service/StockSvc/getKDRSecnInfo"
-api_service_key_stock = "RXhGWArdgsytKaKf0g%2FWxNuo27wXxg4iChLUs9ePc39VvneddFbQ9v9ZXCDWJkdFbhqCvbw9kdMGy%2F%2Bv3it50A%3D%3D"
-api_decode_key_stock = requests.utils.unquote(api_service_key_stock, encoding="utf-8")
-client_id_search = "4NnYXQRzNVwTEO2_rwpd"
-client_secret_search = "mZP8JBDOBK"
+#각 크롤링 결과 저장하기 위한 리스트 선언 
+title_text=[]
+link_text=[]
+source_text=[]
+date_text=[]
+contents_text=[]
+result={}
 
-# 
+#엑셀로 저장하기 위한 변수
+RESULT_PATH ='C:/Users/hyeon/Desktop/naver_news_crawling-master/result'  #결과 저장할 경로
+now = datetime.now() #파일이름 현 시간으로 저장하기
 
-def logging_time(original_fn):
-    def wrapper_fn(*args, **kwargs):
-        start_time = time.time()
-        result = original_fn(*args, **kwargs)
-        end_time = time.time()
-        print(
-            "WorkingTime[{}]: {} sec".format(
-                original_fn.__name__, end_time - start_time
-            )
-        )
-        return result
-
-    return wrapper_fn
-
-
-@logging_time
-def getStockCode(market, url_param):
-    """
-    market: 상장구분 (11=유가증권, 12=코스닥, 13=K-OTC, 14=코넥스, 50=기타비상장)
-    """
-    url_base = f"http://api.seibro.or.kr/openapi/service/{url_param}"
-    url_spec = "getShotnByMartN1"
-    url = url_base + "/" + url_spec
-    api_key_decode = requests.utils.unquote(api_decode_key_stock, encoding="utf-8")
-
-    params = {
-        "serviceKey": api_key_decode,
-        "pageNo": 1,
-        "numOfRows": 100000,
-        "martTpcd": market,
-    }
-
-    response = requests.get(url, params=params)
-    # print(response.text)
-    xml = BeautifulSoup(response.text, "lxml")
-    items = xml.find("items")
-    item_list = []
-    for item in items:
-        item_list.append(item.find("korsecnnm").text.strip())
-
-    return item_list
+#날짜 정제화 함수
+def date_cleansing(test):
+    try:
+        #지난 뉴스
+        #머니투데이  10면1단  2018.11.05.  네이버뉴스   보내기  
+        pattern = '\d+.(\d+).(\d+).'  #정규표현식 
+    
+        r = re.compile(pattern)
+        match = r.search(test).group(0)  # 2018.11.05.
+        date_text.append(match)
+        
+    except AttributeError:
+        #최근 뉴스
+        #이데일리  1시간 전  네이버뉴스   보내기  
+        pattern = '\w* (\d\w*)'     #정규표현식 
+        
+        r = re.compile(pattern)
+        match = r.search(test).group(1)
+        #print(match)
+        date_text.append(match)
 
 
-def search(stock):
-    encText = urllib.parse.quote(stock)  # 검색할 키워드
-    url = (
-        "https://openapi.naver.com/v1/search/news?query=" + encText
-    )  # json 결과가 필요할 때 사용
-    # url = "https://openapi.naver.com/v1/search/news.xml?query=" + encText # xml 결과가 필요할 때 사용
+#내용 정제화 함수 
+def contents_cleansing(contents):
+    first_cleansing_contents = re.sub('<dl>.*?</a> </div> </dd> <dd>', '', 
+                                      str(contents)).strip()  #앞에 필요없는 부분 제거
+    second_cleansing_contents = re.sub('<ul class="relation_lst">.*?</dd>', '', 
+                                       first_cleansing_contents).strip()#뒤에 필요없는 부분 제거 (새끼 기사)
+    third_cleansing_contents = re.sub('<.+?>', '', second_cleansing_contents).strip()
+    contents_text.append(third_cleansing_contents)
+    #print(contents_text)
+    
 
-    request = urllib.request.Request(url)
-    request.add_header("X-Naver-Client-Id", client_id_search)
-    request.add_header("X-Naver-Client-Secret", client_secret_search)
-    response = urllib.request.urlopen(request)
-    rescode = response.getcode()
-    if rescode == 200:
-        response_body = response.read()
-        return response_body.decode("utf-8")
-    else:
-        print("Error Code:" + rescode)
+def crawler(maxpage,query,sort,s_date,e_date):
 
+    s_from = s_date.replace(".","")
+    e_to = e_date.replace(".","")
+    page = 1  
+    maxpage_t =(int(maxpage)-1)*10+1   # 11= 2페이지 21=3페이지 31=4페이지  ...81=9페이지 , 91=10페이지, 101=11페이지
+    
+    while page <= maxpage_t:
+        url = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort="+sort+"&ds=" + s_date + "&de=" + e_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start=" + str(page)
+        
+        response = requests.get(url)
+        html = response.text
+ 
+        #뷰티풀소프의 인자값 지정
+        soup = BeautifulSoup(html, 'html.parser')
+ 
+        #<a>태그에서 제목과 링크주소 추출
+        atags = soup.select('.news_tit')
+        for atag in atags:
+            title_text.append(atag.text)     #제목
+            link_text.append(atag['href'])   #링크주소
+            
+        #신문사 추출
+        source_lists = soup.select('.info_group > .press')
+        for source_list in source_lists:
+            source_text.append(source_list.text)    #신문사
+        
+        #날짜 추출 
+        date_lists = soup.select('.info_group > span.info')
+        for date_list in date_lists:
+            # 1면 3단 같은 위치 제거
+            if date_list.text.find("면") == -1:
+                date_text.append(date_list.text)
+        
+        #본문요약본
+        contents_lists = soup.select('.news_dsc')
+        for contents_list in contents_lists:
+            contents_cleansing(contents_list) #본문요약 정제화
+        
 
-def create_url(stock):
-    encText = urllib.parse.quote(stock)  # 검색할 키워드
-    url = (
-        "https://search.naver.com/search.naver?where=news&sm=tab_pge&query="
-        + encText
-        + "&start="
-        + str(1)
-    )
-    return url
-
-
-def search_crawl(result, urll):
-    original_html = requests.get(urll)
-    html = BeautifulSoup(original_html.text, "html.parser")
-    # 검색결과
-    articles = html.select("div.group_news > ul.list_news > li div.news_area > a")
-    # print(articles)
-    # 검색된 기사의 갯수
-    # print(len(articles), "개의 기사가 검색됨.")
-    # 뉴스기사 제목 가져오기
-    news_title = []
-    news_url = []
-    for i in articles:
-        news_title.append(i.attrs["title"])
-        news_url.append(i.attrs["href"])
-    # all_news.append(news_title)
-    # 뉴스기사 URL 가져오기
-    result.append(news_title)
-
-    # print(news_title, news_url)
-
-
-# cospi = getStockCode(11, "StockSvc")
-# print(cospi)
-# cosdak = getStockCode(12, "StockSvc")
+        #모든 리스트 딕셔너리형태로 저장
+        result= {"date" : date_text , "title":title_text ,  "source" : source_text ,"contents": contents_text ,"link":link_text }  
+        #print(page)
+        
+        df = pd.DataFrame(result)  #df로 변환
+        print(result['title'])
+        page += 10
+    
+    
+    # 새로 만들 파일이름 지정
+    outputFileName = '%s-%s-%s  %s시 %s분 %s초 merging.xlsx' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+    #df.to_excel(RESULT_PATH+outputFileName,sheet_name='sheet1')
+    
+    
 cospi = [
     "동화약품",
     "케이알모터스",
@@ -1055,27 +1054,16 @@ cospi = [
     "신한서부티엔디위탁관리부동산투자회사",
 ]
 
-# if __name__ == "__main__":
-
-
-def run():
-    start = time.time()
-    urls = []
-    for i in cospi:
-        urls.append(create_url(i))
-        
-    pool = Pool(4)
-    m = Manager()
-    result = m.list()
-        
-    pool.starmap(search_crawl, [(result, url) for url in urls])
-    pool.close()
-    pool.join()
-
-    end = time.time()
-    print(len(result))
-    print(f"{end - start:.5f} sec")
-
-if __name__ == "__main__":
-    run()
-
+def main():
+    info_main = input("="*50+"\n"+"입력 형식에 맞게 입력해주세요."+"\n"+" 시작하시려면 Enter를 눌러주세요."+"\n"+"="*50)
+    
+    maxpage = input("최대 크롤링할 페이지 수 입력하시오: ")  
+    #query = input("검색어 입력: ")  
+    sort = input("뉴스 검색 방식 입력(관련도순=0  최신순=1  오래된순=2): ")    #관련도순=0  최신순=1  오래된순=2
+    s_date = input("시작날짜 입력(2019.01.04):")  #2019.01.04
+    e_date = input("끝날짜 입력(2019.01.05):")   #2019.01.05
+    
+    # for query in cospi:
+    #     crawler(maxpage,query,sort,s_date,e_date) 
+    crawler(maxpage,cospi[5],sort,s_date,e_date) 
+main()
