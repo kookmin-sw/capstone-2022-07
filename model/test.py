d2e979d09ba96fe4fdf14d7441af8385464fa382
ls-jun <tndwns19@gmail.com>
2022-04-04 03:12:34 +0900

    test

--- model/test.py
+++ model/test.py
@@ -12,6 +12,8 @@ from datetime import datetime, timedelta
 import codecs
 import re
 
+from zmq import THREAD_AFFINITY_CPU_REMOVE
+
 url = "http://api.seibro.or.kr/openapi/service/StockSvc/getKDRSecnInfo"  # 공공데이터포털 api 주소(Without param)
 api_service_key_stock = "RXhGWArdgsytKaKf0g%2FWxNuo27wXxg4iChLUs9ePc39VvneddFbQ9v9ZXCDWJkdFbhqCvbw9kdMGy%2F%2Bv3it50A%3D%3D"  # service api key
 api_decode_key_stock = requests.utils.unquote(
@@ -79,65 +81,50 @@ def text_clean(inputString, query):
     
     return inputString
 
-
+######## https://haries.tistory.com/4
 # 크롤링 함수
 def search_crawl(tuple_list,query):
     # 삭제할 키워드들 
     del_list = ["오늘의", "뉴스", "급락주","마감","주요","급등주"]
 
-    page = 1
-    maxpage = 3
-    # 11= 2페이지 21=3페이지 31=4페이지  ...81=9페이지 , 91=10페이지, 101=11페이지
-    maxpage_t = (int(maxpage) - 1) * 10 + 1
-    sort = 0 #0=관련도순 1=최신순 
-    while page <= maxpage_t:
+    with open("./negative_words.txt", encoding='utf-8') as neg:
+        negative = neg.readlines()
+    negative = [neg.replace("\n", "") for neg in negative]
+
+    with open("./positive_words.txt", encoding='utf-8') as pos:
+        positive = pos.readlines()
+    positive = [pos.replace("\n", "") for pos in positive]
+
+    news_name =""
+    
+
+    for page_num in range(1,11):
         url = (
-            "https://search.naver.com/search.naver?where=news&query=\""
+            "https://finance.naver.com/item/news_news.nhn?code="
+            
             + query
-            + "\"&sort="
-            + str(sort)
-            + "&start="
-            + str(page)
+            + "&page="
+            + str(page_num)
+            + "&sm=title_entity_id.basic&clusterId="
         )
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
         }
         response = requests.get(url, headers=headers)
-        html = response.text
-
-        # 뷰티풀소프의 인자값 지정
-        soup = BeautifulSoup(html, "html.parser")
-        atags = soup.select(".news_tit")
-        news_name =""
-        news_date =""
-        news_url = ""
-        pov_or_neg = 0 #긍부정 라벨링 값
+        soup = BeautifulSoup(response,'html.parser')
         
-        #긍부정 단어장 오픈
-        with open("./negative_words.txt", encoding='utf-8') as neg:
-            negative = neg.readlines()
-        negative = [neg.replace("\n", "") for neg in negative]
-    
-        with open("./positive_words.txt", encoding='utf-8') as pos:
-            positive = pos.readlines()
-        positive = [pos.replace("\n", "") for pos in positive]
+        all_tr = soup.findAll('td', {'class' : 'title'})
 
+        
+        for tr in all_tr:
+            news_name = tr.find('a').get_text()
+            pov_or_neg = 0 #긍부정 라벨링 값
+            label =0
 
-        # <a>태그에서 제목과 링크주소 추출
-        # 기사 제목, 기사url 저장 
-        for atag in atags:
-            # title_text.append(atag.text)  # 제목
-            # link_text.append(atag["href"])  # 링크주소
-            news_name = atag.text
             if any(keyword in news_name for keyword in del_list):
                 continue
             news_name = text_clean(news_name,query)
-            news_url = atag["href"]
-            label =0
-            tag = ""
-            if any(keyword in news_name for keyword in del_list):
-                continue
-
+            
             for i in range(len(positive)):
                 if news_name.find(positive[i]) != -1:
                     label = 1
@@ -156,12 +143,11 @@ def search_crawl(tuple_list,query):
                 pov_or_neg = -1
                 #print("negative", j)
             elif label == 0:
-                pov_or_neg = 0              
+                pov_or_neg = 0    
 
-        # tuple_list.append((query, news_name, news_url, news_date, pov_or_neg))
-            tuple_list.append((news_name, pov_or_neg, query, tag))
 
-        page += 10
+    # tuple_list.append((query, news_name, news_url, news_date, pov_or_neg))
+        tuple_list.append((news_name, pov_or_neg, query, tag))
 
 
 #cospi = getStockCode(11, "StockSvc")
@@ -1148,24 +1134,10 @@ def run():
 
 
 if __name__ == "__main__":
-    cospi = getStockCode(11, "StockSvc")
-    print(cospi)
+    cospi_code = getStockCode(11, "StockSvc")
+    print(cospi_code)
+    tuple_list=[]
+    for code in cospi_code[:1]:
+        search_crawl(tuple_list,code)
 
-
-    # tuple = run()
-    # with open('./train.csv', 'w') as f:
-    #     writer = csv.writer(f , lineterminator='\n')
-    #     for tup in tuple:
-    #         writer.writerow(tup)
-
-
-
-
-    url = 'http://api.seibro.or.kr/openapi/service/StockSvc/getKDRSecnInfo'
-    api_service_key_stock = "w9Ra19Zqn3+Lgg2zHoRiZa8zZPdSCXSgFgrgFGUkaYqqQRD6BVKMsUgiRyJqeEuG1pQ86vSioq03IRarAve7sg=="  # service api key
-    api_key_decode = requests.utils.unquote(api_decode_key_stock, encoding="utf-8")
-
-    params ={'serviceKey' : api_service_key_stock, 'caltotMartTpcd' : '11' }
-
-    response = requests.get(url, params=params)
-    print(response.content)
+    print(tuple_list)
